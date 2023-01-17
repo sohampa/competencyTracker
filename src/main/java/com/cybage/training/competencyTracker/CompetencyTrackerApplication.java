ec661bd22ed164249a57f639994ede210b9a8806
Mangesh Arun Suryawanshi <mangeshsu@cybage.com>
2023-01-17 10:13:55 +0530

    first commit

---
+++ src/main/java/com/cybage/training/competencyTracker/CompetencyTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.cybage.training.competencyTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompetencyTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompetencyTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cybage/training/competencyTracker/CompetencyTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cybage.training.competencyTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompetencyTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
